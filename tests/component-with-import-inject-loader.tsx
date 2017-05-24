5c456661f59922d85f647802c224f7bab9c41d5d
Florian Richter <florian.richter@mercateo.com>
2017-05-24 15:06:12 +0200

    Use inject-import-loader

--- tests/component-with-import-inject-loader.tsx
+++ tests/component-with-import-inject-loader.tsx
@@ -2,11 +2,17 @@ import expect from 'expect';
 import React from 'react';
 import { shallow } from 'enzyme';
 import { nextTick } from './utils';
-const { Add, ilOverwriteDefaultAdd, resetAllInjects: resetAllInAdd }  = require('../import-inject-loader?defaultAdd!../src/component-with-add-prop');
-const { FetchUser, ilOverwriteFetch, resetAllInjects: resetAllInFetchUser } = require('../import-inject-loader?fetch!../src/component-with-fetching');
+const { Add, ilOverwriteDefaultAdd, resetAllInjects: resetAllInAdd }  = require('import-inject-loader?defaultAdd!../src/component-with-add-prop');
+const { FetchUser, ilOverwriteFetch, resetAllInjects: resetAllInFetchUser } = require('import-inject-loader?fetch!../src/component-with-fetching');
 
 describe('component: `<FetchUser />` with import-inject-loader', () => {
-  it('should fetch user with replaced fetch and magic val', async () => {
+  beforeEach(() => {
+    // could be done at the end of the second test only as well,
+    // but as this wouldn't be executed for tests with exceptions we should call it before each test individually
+    resetAllInFetchUser();
+  });
+
+  it('should fetch user with replaced fetch', async () => {
     ilOverwriteFetch((url: string): Promise<any> => {
       return new Promise((resolve, reject) => {
         resolve({
@@ -21,23 +27,29 @@ describe('component: `<FetchUser />` with import-inject-loader', () => {
 
     expect(getText()).toBe('Loading...');
     await nextTick();
-    expect(getText()).toBe('Hello Mocked Foo Bar!');
 
-    resetAllInFetchUser();
+    expect(getText()).toBe('Hello Mocked Foo Bar!');
   });
 
-  it('should fetch user with default impl', async () => {
+  it('should fetch user with default fetch', async () => {
     const wrapper = shallow(<FetchUser />);
     const getText = () => wrapper.find('p').text();
 
     expect(getText()).toBe('Loading...');
     await nextTick();
+
     // Keeps loading, as the Promise doesn't get resolved
     expect(getText()).toBe('Loading...');
   });
 });
 
 describe('component: `<Add />` with import-inject-loader', () => {
+  beforeEach(() => {
+    // could be done at the end of the second test only as well,
+    // but as this wouldn't be executed for tests with exceptions we should call it before each test individually
+    resetAllInAdd();
+  });
+
   it('should show the sum', () => {
     const wrapper = shallow(<Add a={1} b={2} />);
 
@@ -45,18 +57,16 @@ describe('component: `<Add />` with import-inject-loader', () => {
     expect(wrapper.text()).toBe('The sum is: 3.');
   });
 
-  it('should show the product because of function replacement', () => {
+  it('should show the product', () => {
     ilOverwriteDefaultAdd((a: number, b: number) => a * b);
 
     const wrapper = shallow(<Add a={1} b={2} />);
 
     expect(wrapper.type()).toBe('p');
     expect(wrapper.text()).toBe('The sum is: 2.');
-
-    resetAllInAdd();
   });
 
-  it('should show the sum like before', () => {
+  it('should show the sum as before', () => {
     const wrapper = shallow(<Add a={7} b={8} />);
 
     expect(wrapper.type()).toBe('p');
