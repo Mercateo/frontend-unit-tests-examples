56e50a2c43cc9889c7d063eafaa2ce1e003bc4dc
Philipp Zins <philipp.zins@mercateo.com>
2017-06-02 10:29:36 +0200

    small deprecation fixes

--- tests/component-without-enzyme.tsx
+++ tests/component-without-enzyme.tsx
@@ -1,11 +1,11 @@
 import expect from 'expect';
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import { Add } from '../src/component';
 
 describe('component: `<Add />` without enzyme', () => {
   it('should show the sum', () => {
-    const renderer = ReactTestUtils.createRenderer();
+    const renderer = TestUtils.createRenderer();
     renderer.render(<Add a={1} b={2} />);
     const output = renderer.getRenderOutput();
 
